c3869f0dd7da13a10d89751d816f453cffb21568
Austin Karalekas <akaralekas@cloudcollective.com>
2022-04-08 13:32:04 -0400

    New

--- Scripts/CheckOU.ps1
+++ Scripts/CheckOU.ps1
@@ -5,3 +5,5 @@ Search-ADAccount -AccountDisabled -UsersOnly | Move-ADObject -TargetPath $OU -wh
 //Search-ADAccount –AccountDisabled –UsersOnly –SearchBase "DC=Comp,DC=Any" | Where-Object {$_.distinguishedname -notlike "*,OU=ExcludeThis,*"} | Move-ADObject –TargetPath "OU=Disabled Accounts,DC=Comp,DC=Any"
 
 //Instead of moving, make it grab the users as an array, store in variable. Check both users in Archived and Disabled and check for enabled users in disabled OU.
+
+new change
