d48551d3abf9afbced51893bcf17ef86c8d899ae
Austin Karalekas <akaralekas@cloudcollective.com>
2022-04-08 13:22:53 -0400

    init

---
+++ Scripts/Bearandlearn.py
@@ -0,0 +1,5 @@
+from py_compile import _get_default_invalidation_mode
+
+
+username = "Gundam" 
+print(username)
---
+++ Scripts/CheckOU.ps1
@@ -0,0 +1,7 @@
+$OU = "OU=Users,OU=Archived or Disabled,*"
+
+Search-ADAccount -AccountDisabled -UsersOnly | Move-ADObject -TargetPath $OU -whatif
+
+//Search-ADAccount –AccountDisabled –UsersOnly –SearchBase "DC=Comp,DC=Any" | Where-Object {$_.distinguishedname -notlike "*,OU=ExcludeThis,*"} | Move-ADObject –TargetPath "OU=Disabled Accounts,DC=Comp,DC=Any"
+
+//Instead of moving, make it grab the users as an array, store in variable. Check both users in Archived and Disabled and check for enabled users in disabled OU.
